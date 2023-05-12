5c60ebb055b2cf8229e787ce378cff8cfdc97416
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-12 16:48:33 +0100

    created a small Mad libs generator

--- mad-libs.py
+++ mad-libs.py
@@ -1,7 +1,23 @@
-print("Welcome to my Mad Libs Generagtor!")
+def madlibs():
 
-noun = input("Please enter a Noun: ")
-adjective = input("Please enter a Adjective: ")
-verb = input("Please enter a Verb: ")
-adverb = input("Please enter a Adverb: ")
-interjection = input("Please enter a Interjection: ")
+    # Print welcome message
+    print("Welcome to my Mad Libs Generagtor!")
+
+    # Get user input
+    noun = input("Please enter a Noun: ")
+    adjective = input("Please enter a Adjective: ")
+    verb = input("Please enter a Verb: ")
+    adverb = input("Please enter a Adverb: ")
+    interjection = input("Please enter a Interjection: ")
+
+    # Print the final Mad Lib with the users input
+    print(f"""
+          Once upon a time, in a faraway land, there lived a brave {noun}. This {adjective} hero had the power to 
+          {verb} {adverb} and protect the innocent. One day, while wandering through the enchanted forest, the hero 
+          suddenly heard a rustling sound. "Oh {interjection}!" exclaimed the hero, ready to face any danger that 
+          lay ahead. With {adverb} speed, the hero dashed towards the noise, holding their {noun} tightly. Little 
+          did they know, a thrilling adventure awaited them!
+          """)
+
+madlibs()
+
