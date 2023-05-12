545d139c4e5b89b5fac1eae047f0ea3349a4ed64
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-05-12 16:30:05 +0100

    first commit

---
+++ mad-libs.py
@@ -0,0 +1,7 @@
+print("Welcome to my Mad Libs Generagtor!")
+
+noun = input("Please enter a Noun: ")
+adjective = input("Please enter a Adjective: ")
+verb = input("Please enter a Verb: ")
+adverb = input("Please enter a Adverb: ")
+interjection = input("Please enter a Interjection: ")
